355509886846b50911489a6ff678bf5d009fbf0a
KoniDevs <delgadoargie04@gmail.com>
2024-09-02 05:45:25 +0800

    feat: add production origin CORS

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(passportSetup.initialize());
 app.use(passportSetup.session());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://dcs-cz15.onrender.com"],
+    origin: ["http://localhost:5173", "https://documentcontrollersystem.onrender.com"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
