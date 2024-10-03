278ab22c950364fb3c4cd0a85f41068a491ecaa7
KoniDevs <delgadoargie04@gmail.com>
2024-10-03 15:16:55 +0800

    chore: modify registry routes

--- server/routes/registryRoutes.js
+++ server/routes/registryRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/', getAllRegistry);
 router.get('/:id', getRegistryByID);
 router.post('/', saveRegistry);
-router.put('/:id', editRegistry);
+router.patch('/:id', editRegistry);
 router.delete('/:id', deleteRegistry);
 
 export default router;
