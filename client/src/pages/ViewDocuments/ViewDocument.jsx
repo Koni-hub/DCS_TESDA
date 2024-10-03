493eceaff4f97969870c7ee95ca6d9f8867bdcbd
KoniDevs <delgadoargie04@gmail.com>
2024-10-03 15:14:43 +0800

    chore: modify formatDate

--- client/src/pages/ViewDocuments/ViewDocument.jsx
+++ client/src/pages/ViewDocuments/ViewDocument.jsx
@@ -137,8 +137,6 @@ const ViewDocument = ({ normalAccount, googleAccount }) => {
       year: 'numeric',
       month: 'short',
       day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
     };
     return date.toLocaleDateString('en-US', options);
   };
