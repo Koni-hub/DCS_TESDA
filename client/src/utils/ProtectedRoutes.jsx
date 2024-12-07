a99b646508ef9c72615f6bbcffa4467298fb429a
koni-hub <delgadoargie04z@gmail.com>
2024-12-07 17:19:00 +0800

    chore: clean up unused code

--- client/src/utils/ProtectedRoutes.jsx
+++ client/src/utils/ProtectedRoutes.jsx
@@ -1,7 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoutes() {
-  // Retrieve from local storage if states are not set
   const isLoggedIn = window.localStorage.getItem('loggedIn') === 'true';
   const token = window.localStorage.getItem('token');
   const role = window.localStorage.getItem('role');
@@ -10,7 +9,6 @@ function ProtectedRoutes() {
   console.log('Token:', token);
   console.log('Role:', role);
 
-  // Check if the user is logged in and has a valid role
   const isAuthorized =
     isLoggedIn &&
     token &&
